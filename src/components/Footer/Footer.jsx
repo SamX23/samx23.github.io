c0a0e014743b72bd970b0f8264c2a7609f41267e
Sami Kalammallah <skalexsong@gmail.com>
2020-09-07 17:08:03 +0700

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,10 @@ import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
-
-import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -38,13 +34,10 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
-          </a>
+          This template is using <a href="https://github.com/cobidev/gatsby-simplefolio" target="_blank" rel="noopener noreferrer">
+            Gatsby simplefolio
+          </a> - Modified with ❤️ by Sami Kalammallah © {new Date().getFullYear()}
         </p>
-
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
